44eddb54b7709610cc1afc9cb6059c720fabac7a
srijithit <srijithit@users.noreply.github.com>
2026-08-27 12:35:11 +0530

    Include default upload image assets for static serving on Render

---
+++ public/uploads/index.js
@@ -0,0 +1,410 @@
+// src/assets/images/index.js
+import journeyImg from './family_portrait.webp';
+import journey_slide_1 from './journey_slide_1_1.webp';
+import journey_slide_2 from './journey_slide_2_1.webp';
+import journey_slide_3 from './journey_slide_3_1.webp';
+import journey_slide_4 from './journey_slide_4.webp';
+import journey_slide_5 from './journey_slide_5.webp';
+import journey_slide_6 from './journey_slide_6.webp';
+import journey_slide_7 from './journey_slide_7.webp';
+import family_portrait from './family_portrait.webp';
+import central_kitchen from './central_kitchen.webp';
+import dubai_milestone from './dubai_milestone.webp';
+import chennai_largest from './chennai_largest.webp';
+
+
+import home_marquee_img1 from './home_marquee_img1.webp';
+import home_marquee_img2 from './home_marquee_img2.webp';
+import home_marquee_img3 from './home_marquee_img3.webp';
+import home_marquee_img4 from './home_marquee_img4.webp';
+
+import experience_card1 from './experience_card1.webp';
+import experience_card2 from './experience_card_2_m.webp';
+import experience_card3 from './experience_card3.webp';
+import experience_card4 from './experience_card4.webp';
+
+import experience_card1_m from './experience_card1_m.webp';
+import experience_card2_m from './experience_card2_m.webp';
+import experience_card_2_m from './experience_card_2_m.webp';
+import experience_card3_m from './experience_card3_m.webp';
+import experience_card4_m from './experience_card4_m.webp';
+
+import vbCardLogoTrans1 from './vb-card-logo-trans-1.webp';
+import vbCardLogoTrans2 from './vb-card-logo-trans-2.webp';
+import vbCardLogoTrans3 from './vb-card-logo-trans-3.webp';
+import vbCardLogoTrans4 from './vb-card-logo-trans-3.webp';
+import vb_world_logo_black_text from './vb_world_logo_black_text.webp'
+
+import menuheroBanner from './menuhero-banner.webp'
+import menuctaBanner from './menucta-banner.webp'
+import homeheroBanner from './homehero-banner.webp'
+import homeCtaBanner from './homecta-banner.webp'
+import aboutheroBanner from './abouthero-banner.webp'
+import aboutctaBanner from './aboutcta-banner.webp'
+import branchheroBanner from './branchhero-banner.webp'
+import privacyHeroBanner from './privacy-hero-bg.webp'
+import privacyCtaBanner from './privacycta-banner.webp'
+import termsHeroBanner from './termshome-banner.webp'
+import brandhome from './brandhome.webp'
+import brandhome_img from './brandhome.webp'
+import brandheroBanner from './brandhome.webp'
+import brandctaBanner from './brand_cta_banner_2.webp'
+import banquethallheroBanner from './banquethallhero-banner.webp'
+import banquethallCtaBanner from './banquet-hallscta-banner.webp'
+
+import aboutheroBanner_m from './abouthero-banner_m.webp'
+import menuheroBanner_m from './menuhero-banner_m.webp'
+import brandheroBanner_m from './brandhero-banner_m.webp'
+import homeheroBanner_m from './homehero-banner_m.webp'
+import homehero_banner_2 from './homehero_banner_2.webp'
+import homehero_banner_2_m from './homehero_banner_2_m.webp'
+import homehero_banner_3 from './homehero_banner_3.webp'
+import homehero_banner_3_m from './homehero_banner_3_m.webp'
+import homehero_banner_4 from './homehero_banner_4.webp'
+import homehero_banner_4_m from './homehero_banner_4_m.webp'
+import branchheroBanner_m from './branchhero-banner_m.webp'
+import banquethallheroBanner_m from './banquethallhero-banner_m.webp'
+import banquet_venue_saligramam from './banquet_venue_saligramam.webp'
+import banquet_venue_saligramam_m from './banquet_venue_saligramam_m.webp'
+
+import brandLegacyMain from './brand-legacy-main.webp'
+import brandLegacySecondary from './brand-legacy-secondary.webp'
+import brandLegacyBoth from './brand-legacy-both.webp'
+import banquethallsBoth from './banquet-halls-both.webp'
+
+
+import brandShowcaseVbworld from './brand-showcase-vbworld.webp'
+import brandShowcaseNvvb from './brand-showcase-nvvb.webp'
+import brandShowcaseConsbrew from './brand-showcase-consbrew.webp'
+
+import privacySubImg from './privacy-sub-img.webp'
+import termsSubImg from './terms-sub-img.webp'
+
+
+import aboutFamily from './aboutfamily-new.webp'
+import about_1 from './about_1.webp'
+import about_2 from './about_2.webp'
+import about_3 from './about_3.webp'
+
+
+import about_marquee_img1 from './about_marquee_img1.webp'
+import about_marquee_img2 from './about_marquee_img2.webp'
+import about_marquee_img3 from './about_marquee_img3.webp'
+import about_marquee_img4 from './about_marquee_img4.webp'
+import about_marquee_img5 from './about_marquee_img5.webp'
+
+import about_heritage from './about_heritage.webp'
+import about_excellence from './about_excellence.webp'
+import about_scale from './about_scale.webp'
+import about_presence from './about_presence.webp'
+import about_innovation from './about_innovation.webp'
+import about_impact from './about_impact.webp'
+import about_vision from './about_vision.webp'
+
+import about_slider_img1 from './about_slider_img1.webp'
+import about_slider_img2 from './about_slider_img2.webp'
+import about_slider_img3 from './about_slider_img3.webp'
+
+import about_exp_img1 from './about-exp-img1.webp'
+import about_exp_img2 from './about-exp-img2.webp'
+import about_exp_img3 from './about-exp-img3.webp'
+
+import initial_consultation from './initial_consultation.webp'
+import culinary_space_design from './culinary_space_design.webp'
+import banquet_grand_wedding from './banquet_grand_wedding.webp'
+import banquet_people_eating from './banquet_people_eating.webp'
+import flawless_execution from './flawless_execution.webp'
+
+import menu_cate_bottom from './menu_cate_bottom.webp'
+import menuRose from './menu-rose.webp'
+
+import menu_cate_img1 from './menu_cate_img1.webp'
+
+import menu_cate_img2 from './about-exp-img2.webp'
+import menu_cate_img3 from './about-exp-img3.webp'
+import menu_cate_img4 from './about_marquee_img1.webp'
+import menu_cate_img5 from './about_marquee_img2.webp'
+
+
+import menu_mrng_product from './menu_mrng_product.webp'
+import menu_noon_product from './menu_noon_product.webp'
+import menu_evening_product from './menu_eve_product.webp'
+
+import branch_section_img1 from './branch-section-img1_1.webp'
+import branch_section_img2 from './branch-section-img2.webp'
+
+import brand_diff_img1 from './brand-diff-img1.webp'
+import brand_diff_img2 from './brand-diff-img2.webp'
+import brand_diff_img3 from './brand-diff-img3.webp'
+import brand_diff_img4 from './brand-diff-img4.webp'
+
+import banquethallsMorethanImg1 from './banquet-halls-morethan-img1.webp'
+import banquethallsMorethanImg2 from './banquet-halls-morethan-img2.webp'
+import banquethallsMorethanImg3 from './banquet-halls-morethan-img3.webp'
+
+import banquethallsCelebration from './banquet-halls-celebration.webp'
+import banquethallsCeleImg1 from './banquet-halls-cele-img1.webp'
+import banquethallsCeleImg2 from './banquet-halls-cele-img2.webp'
+import banquethallsCeleImg3 from './banquet-halls-cele-img3.webp'
+import banquethallsCeleImg4 from './banquet-halls-cele-img4.webp'
+
+import homeLegacyBuiltImg1 from './home-legacy-built-img1.webp'
+import homeLegacyBuiltImg2 from './home-legacy-built-img2.webp'
+import homeLegacyBuiltImg3 from './home-legacy-built-img3.webp'
+import homeLegacyBuiltImg4 from './home-legacy-built-img4.webp'
+import homeLegacyBuiltImg5 from './home-legacy-built-img5.webp'
+import homeLegacyBuiltImg6 from './home-legacy-built-img6.webp'
+import homeLegacyBuiltImg7 from './home-legacy-built-img7.webp'
+import homeLegacyBuiltImg8 from './home-legacy-built-img8.webp'
+import homeLegacyBuiltImg9 from './home-legacy-built-img9.webp'
+
+import home_awards_img1 from './home_awards_img1.webp'
+import home_awards_img2 from './home_awards_img2.webp'
+import home_awards_img3 from './home_awards_img3.webp'
+import home_awards_img4 from './home_awards_img4.webp'
+import home_awards_img5 from './home_awards_img5.webp'
+import home_awards_img6 from './home_awards_img6.webp'
+import home_awards_img7 from './home_awards_img7.webp'
+import home_awards_img8 from './home_awards_img8.webp'
+
+import sweet_img1 from './sweet_img1.webp'
+import sweet_img2 from './sweet_img2.webp'
+import sweet_img3 from './sweet_img3.webp'
+
+import menu_south_indian_special from './menu_south_indian_special.webp'
+import menu_dum_biryani from './menu_dum_biryani.webp'
+import menu_tandoor_kebabs from './menu_tandoor_kebabs.webp'
+import menu_pan_asian from './menu_pan_asian.webp'
+import menu_noodles_rice from './menu_noodles_rice.webp'
+import menu_european from './menu_european.webp'
+import menu_south_indian_meals from './menu_south_indian_meals.webp'
+import menu_vb_special from './menu_vb_special.webp'
+import menu_continental_cuisine from './menu_continental_cuisine.webp'
+import branch_uk from './branch_uk.webp'
+import branch_hosur from './branch_hosur.webp'
+import branch_annanagar from './branch_annanagar.webp'
+import branch_kanchipuram from './branch_kanchipuram.webp'
+import branch_dubairaffa from './branch_dubairaffa.webp'
+import branch_tambaram from './branch_tambaram.webp'
+import branch_velachery from './branch_velachery.webp'
+import branch_vadapalani from './branch_vadapalani.webp'
+import branch_egmore1 from './branch_egmore1.webp'
+import branch_egmore2 from './branch_egmore2.webp'
+import people_eating_fallback from './people_eating_fallback.webp'
+import coming_soon_branch from './coming_soon_branch.webp'
+import branch_vikravandi from './branch_vikravandi.webp'
+import branch_nexusvijayamall from './branch_nexusvijayamall.webp'
+import branch_neelankarai from './branch_neelankarai.webp'
+import branch_maduravoyal from './branch_maduravoyal.webp'
+import branch_saligramam from './branch_saligramam.webp'
+import branch_100ft from './branch_100ft.webp'
+import branch_airport from './branch_airport.webp'
+import branch_chromepet from './branch_chromepet.webp'
+import branch_medavakkam from './branch_medavakkam.webp'
+import branch_mylapore from './branch_mylapore.webp'
+import branch_phoenixmall from './branch_phoenixmall.webp'
+import branch_singapore from './branch_singapore.webp'
+import branch_sharjah from './branch_sharjah.webp'
+import branch_tnagar from './branch_tnagar.webp'
+
+export const IMAGES = {
+  journeyImg,
+  journey_slide_1,
+  journey_slide_2,
+  journey_slide_3,
+  family_portrait,
+  central_kitchen,
+  dubai_milestone,
+  chennai_largest,
+  journey_slide_4,
+  journey_slide_5,
+  about_heritage,
+  about_excellence,
+  about_scale,
+  about_presence,
+  about_innovation,
+  about_impact,
+  about_vision,
+  journey_slide_6,
+  initial_consultation,
+  culinary_space_design,
+  banquet_grand_wedding,
+  banquet_people_eating,
+  flawless_execution,
+  journey_slide_7,
+
+  home_marquee_img1,
+  home_marquee_img2,
+  home_marquee_img3,
+  home_marquee_img4,
+
+  experience_card1,
+  experience_card2,
+  experience_card3,
+  experience_card4,
+
+  experience_card1_m,
+  experience_card_2_m,
+  experience_card2_m,
+  experience_card3_m,
+  experience_card4_m,
+
+  vbCardLogoTrans1,
+  vbCardLogoTrans2,
+  vbCardLogoTrans3,
+  vbCardLogoTrans4,
+  vb_world_logo_black_text,
+
+  homeCtaBanner,
+  homeheroBanner,
+  aboutheroBanner,
+  aboutctaBanner,
+  menuheroBanner,
+  menuctaBanner,
+  branchheroBanner,
+  privacyHeroBanner,
+  privacyCtaBanner,
+  termsHeroBanner,
+  brandhome,
+  brandhome_img,
+  brandheroBanner,
+  brandctaBanner,
+  banquethallheroBanner,
+  banquethallCtaBanner,
+  aboutheroBanner_m,
+  menuheroBanner_m,
+  brandheroBanner_m,
+  homeheroBanner_m,
+  branchheroBanner_m,
+  banquethallheroBanner_m,
+
+  brandLegacyMain,
+  brandLegacySecondary,
+  brandLegacyBoth,
+  banquethallsBoth,
+
+  brandShowcaseVbworld,
+  brandShowcaseNvvb,
+  brandShowcaseConsbrew,
+
+  privacySubImg,
+  termsSubImg,
+
+  aboutFamily,
+  about_1,
+  about_2,
+  about_3,
+  menuRose,
+
+  sweet_img1,
+  sweet_img2,
+  sweet_img3,
+
+  about_marquee_img1,
+  about_marquee_img2,
+  about_marquee_img3,
+  about_marquee_img4,
+  about_marquee_img5,
+
+  about_slider_img1,
+  about_slider_img2,
+  about_slider_img3,
+
+  about_exp_img1,
+  about_exp_img2,
+  about_exp_img3,
+
+  menu_cate_bottom,
+
+  menu_cate_img1,
+  menu_cate_img2,
+  menu_cate_img3,
+  menu_cate_img4,
+  menu_cate_img5,
+
+  menu_mrng_product,
+  menu_noon_product,
+  menu_evening_product,
+
+  branch_section_img1,
+  branch_section_img2,
+
+  brand_diff_img1,
+  brand_diff_img2,
+  brand_diff_img3,
+  brand_diff_img4,
+
+  banquethallsMorethanImg1,
+  banquethallsMorethanImg2,
+  banquethallsMorethanImg3,
+
+  banquethallsCelebration,
+  banquethallsCeleImg1,
+  banquethallsCeleImg2,
+  banquethallsCeleImg3,
+  banquethallsCeleImg4,
+
+  homeLegacyBuiltImg1,
+  homeLegacyBuiltImg2,
+  homeLegacyBuiltImg3,
+  homeLegacyBuiltImg4,
+  homeLegacyBuiltImg5,
+  homeLegacyBuiltImg6,
+  homeLegacyBuiltImg7,
+  homeLegacyBuiltImg8,
+  homeLegacyBuiltImg9,
+
+  home_awards_img1,
+  home_awards_img2,
+  home_awards_img3,
+  home_awards_img4,
+  home_awards_img5,
+  home_awards_img6,
+  home_awards_img7,
+  home_awards_img8,
+
+  menu_south_indian_special,
+  menu_dum_biryani,
+  menu_tandoor_kebabs,
+  menu_pan_asian,
+  menu_noodles_rice,
+  menu_european,
+  menu_south_indian_meals,
+  menu_vb_special,
+  menu_continental_cuisine,
+  branch_uk,
+  branch_hosur,
+  branch_annanagar,
+  branch_kanchipuram,
+  branch_dubairaffa,
+  branch_tambaram,
+  branch_velachery,
+  branch_vadapalani,
+  branch_egmore1,
+  branch_egmore2,
+  people_eating_fallback,
+  coming_soon_branch,
+  branch_vikravandi,
+  branch_nexusvijayamall,
+  branch_neelankarai,
+  branch_maduravoyal,
+  branch_saligramam,
+  branch_100ft,
+  branch_airport,
+  branch_chromepet,
+  branch_medavakkam,
+  branch_mylapore,
+  branch_phoenixmall,
+  branch_singapore,
+  branch_sharjah,
+  branch_tnagar,
+  homehero_banner_2,
+  homehero_banner_2_m,
+  homehero_banner_3,
+  homehero_banner_3_m,
+  homehero_banner_4,
+  homehero_banner_4_m,
+  banquet_venue_saligramam,
+  banquet_venue_saligramam_m,
+};
+
